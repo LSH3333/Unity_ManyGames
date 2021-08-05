cc227894b736ac29e487c7c8647571ceadc4b79d
Jake <dltpgustpgus@gmail.com>
2021-08-05 21:23:31 +0900

    Main menu scene moving characters added

--- Assets/MenuScene/CharactersReturnPos.cs
+++ Assets/MenuScene/CharactersReturnPos.cs
@@ -2,12 +2,21 @@
 
 public class CharactersReturnPos : MonoBehaviour
 {
-    public GameObject returnpos;
+    public GameObject birdReturnpos;
+    public GameObject returnPos;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        // 캐릭터들이 화면 좌측끝에 도달하면 오른쪽 끝(returnpos)에 돌아오도록함 
-        collision.gameObject.transform.position = returnpos.transform.position;
+        if(collision.gameObject.name == "MainMenuBird")
+        {
+            // Bird 화면 좌측끝에 도달하면 오른쪽 끝(birdReturnpos)에 돌아오도록함 
+            collision.gameObject.transform.position = birdReturnpos.transform.position;
+        }
+        else
+        {
+            collision.gameObject.transform.position = returnPos.transform.position;
+        }
+       
     }
 
     
---
+++ Assets/MenuScene/MenuSceneMovingCharacters/MenuSwordMan.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuSwordMan : MonoBehaviour
+{
+    public Rigidbody2D rigid;
+
+    private void Start()
+    {
+        rigid.bodyType = RigidbodyType2D.Kinematic;
+        rigid.velocity = new Vector3(-1, 0, 0);
+    }
+
+
+}
