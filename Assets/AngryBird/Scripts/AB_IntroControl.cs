8c4e47f4ebf04e18c683b50e84790266e08f0dd7
Jake <dltpgustpgus@gmail.com>
2021-09-04 19:04:50 +0900

    angryBird intro fixed

--- Assets/AngryBird/Scripts/AB_IntroControl.cs
+++ Assets/AngryBird/Scripts/AB_IntroControl.cs
@@ -5,6 +5,7 @@ public class AB_IntroControl : MonoBehaviour
     public GameObject FirstBomb;
 
     // called when intro ends
+    // Intro 끝난후 첫번째로 날릴 Bomb active 
     public void IntroEnds()
     {
         //Angry_ManagerGame.singleton.gameMode = 1; // game play
