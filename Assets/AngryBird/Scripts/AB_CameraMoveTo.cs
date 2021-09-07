c4e3fd41eb02cdd332b633b484835dddcc2905d2
Jake <dltpgustpgus@gmail.com>
2021-09-07 22:53:11 +0900

    angrybird, positions fixing

---
+++ Assets/AngryBird/Scripts/AB_CameraMoveTo.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AB_CameraMoveTo : MonoBehaviour
+{
+    private Vector3 initCamPos;
+    private Vector3 nextCamPos;
+    private bool toInit = false;
+    private bool trigger = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        initCamPos = new Vector3(0, 0, -10);
+        nextCamPos = new Vector3(16, 0, -10);        
+    }
+
+    private void Update()
+    {
+        if (transform.position.x >= 15.5f && !trigger)
+        {
+            trigger = true;
+            toInit = true;
+        }
+
+        if(toInit)
+            Camera.main.transform.position = Vector3.Lerp(transform.position, initCamPos, Time.deltaTime);
+        else
+            Camera.main.transform.position = Vector3.Lerp(transform.position, nextCamPos, Time.deltaTime);
+    }
+
+}
---
+++ Assets/AngryBird/Scripts/AB_CreateMap.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AB_CreateMap : MonoBehaviour
+{
+    private Vector3 flagLeft, flagRight;
+    public GameObject column, roof;
+
+    private void Start()
+    {
+        flagLeft = new Vector3(14.98f, -1.22f, 0f);
+        flagRight = new Vector3(16.64f, -1.22f, 0f);
+    }
+
+    void SpawnRandomColumn()
+    {
+        // 가운데 두개의 기둥 
+        Instantiate(column, flagLeft, Quaternion.identity);
+        Instantiate(column, flagRight, Quaternion.identity);
+        
+        // 2~6개의 기둥 랜덤 소환 
+        int cnt = Random.Range(2, 7);
+
+        float left = flagLeft.x;
+        float right = flagRight.x;
+        for(int i = 1; i <= cnt; i++)
+        {
+            // 좌측소환 
+            if(i % 2 != 0)
+            {
+                left -= 1.66f; // plank의 긴부분 길이 
+                 
+            }
+            // 우측소환 
+            else
+            {
+
+            }
+        }
+
+    }
+
+}
