bf8158fddc869a1ce9d5ae2cb49bd65a23981ab8
LSH3333 <dltpgustpgus@gmail.com>
2021-12-11 17:18:53 +0900

    AdminBoard 사운드 추가, AreYouSureBox 뒤로가기 버튼 추가

--- Assets/MenuScene/AreYouSureBoxManager.cs
+++ Assets/MenuScene/AreYouSureBoxManager.cs
@@ -3,6 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class AreYouSureBoxManager : MonoBehaviour
-{
+{    
 
+    public void OnClickAreYouSureBoxBackBtn()
+    {
+        GetComponent<Animator>().SetTrigger("popout");
+        StartCoroutine(UnactiveBox());
+
+    }
+
+    private IEnumerator UnactiveBox()
+    {
+        yield return new WaitForSeconds(1f);
+        gameObject.SetActive(false);
+        yield return null;
+    }
 }
