8521bf97a173ed3355185c26391c2a53cda6bebb
LSH <dltpgustpgus@gmail.com>
2023-10-04 20:55:34 +0900

    Jumper sepearate build

--- Assets/PublicResources/SeperateGameBuild.cs
+++ Assets/PublicResources/SeperateGameBuild.cs
@@ -10,7 +10,7 @@ public class SeperateGameBuild : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        ChangeSceneToGame("AngryBird");
+        ChangeSceneToGame("Jumper");
     }
 
     public void ChangeSceneToGame(string gameName)
